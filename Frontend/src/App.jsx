7d0c6a18cc88c6520e66259ed0c9ce414e5e263a
aryan <aryan@vt.edu>
2025-03-19 13:58:10 -0400

    Testing sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
 
-import Homepage from "./pages/Homepage";
-import MainLayout from "./layout/MainLayout";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import NotfoundPage from "./pages/NotfoundPage";
-import Dashboard from "./pages/Dashboard";
+import Homepage from './pages/Homepage'
+import MainLayout from './layout/MainLayout'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import NotfoundPage from './pages/NotfoundPage'
+import Dashboard from './pages/Dashboard'
+import Sidebar from './pages/Sidebar'
 
 const route = createBrowserRouter(
   createRoutesFromElements(
@@ -15,12 +21,13 @@ const route = createBrowserRouter(
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
       <Route path="dashboard" element={<Dashboard />} />
+      <Route path="sidebar" element={<Sidebar />} />
       <Route path="*" element={<NotfoundPage />} />
     </Route>
   )
-);
+)
 
 const App = () => {
-  return <RouterProvider router={route} />;
-};
-export default App;
+  return <RouterProvider router={route} />
+}
+export default App
---
+++ Frontend/src/pages/sidebar.jsx
@@ -0,0 +1,6 @@
+import Sidebar from '../components/Sidebar'
+
+const sidebar = () => {
+  return <Sidebar />
+}
+export default sidebar
