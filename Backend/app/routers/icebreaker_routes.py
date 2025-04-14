b79a5b8f1e96f39481e018621a899e8c0e3b31d4
Kshitij Narvekar <kshitijnarvekar@kshitijs-mbp.mynetworksettings.com>
2025-04-13 21:08:24 -0400

    Bug Temp Fixes

--- Backend/app/routers/icebreaker_routes.py
+++ Backend/app/routers/icebreaker_routes.py
@@ -27,12 +27,19 @@ async def get_icebreaker_activity(request: IceBreakerRequest):
         materials_filter = "No Materials are necessary for this activity."
         disorder="dyslexic"
         setting="In-Person"
-        
-        rag_text = generate_icebreaker(
-            materials=materials_filter,
-            question=question,
-            disorder=disorder
-        )
+
+        # rag_text = generate_icebreaker(
+        #     materials=materials_filter,
+        #     question=question,
+        #     disorder=disorder
+        # )
+        rag_text = """**Title:** Marooned
+**Objective:** To help participants learn to work together for the greater good of the group while also expressing their own individual feelings and perspectives.
+**Materials Needed:** None
+**Instructions:** You are marooned on an island. Your objective is to choose five items you would bring with you if you knew there was a chance that you might be stranded. Rules Note that each team is only allowed five items per team, not per person. Each team must discuss and defend their choices with the whole group.
+**Debrief / Discussion Points:** Was the object you wanted selected by the group? For those of you whose objects weren't selected, how did it make you feel? How did you all determine what objects should and should not be brought? What might this activity symbolize within our own fraternal experiences?
+**Additional Details:** Tips for Success: This activity helps members learn about others' values and problem-solving styles, and promotes teamwork. The value of the items selected to bring should be discussed and compared."""
+
         print(rag_text)
         if not rag_text:
             raise HTTPException(
